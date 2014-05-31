2112cd55f04588a5fea7058b7ac4cc54f3223d8a
a.alexandrakis <a.alexandrakis@datamation.gr>
2014-05-31 19:44:14 +0300

    initial commit

---
+++ src/com/aalexandrakis/kimobile/Login.java
@@ -0,0 +1,15 @@
+package com.aalexandrakis.kimobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}

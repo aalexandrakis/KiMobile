255715551f4ada05909040e20b86fc1596927925
Alexandrakis Alexandros <aalexandrakis@hotmail.com>
2015-04-18 17:21:10 +0300

    change web service url

--- src/com/aalexandrakis/kimobile/Constants.java
+++ src/com/aalexandrakis/kimobile/Constants.java
@@ -4,13 +4,13 @@ public class Constants {
 
 	static final String SHARED_PREFERENCES = "KiMobilePrefs";
 	final static String PROTOCOL = "http://"; //ubuntu & node server
-	final static String HOST = "kimo-aalexandrakis.rhcloud.com"; //ubuntu & node server
+	final static String HOST = "kimo.aalexandrakis.com"; //ubuntu & node server
 //	final static String URL = "http://192.168.1.2:9090"; //ubuntu
 //	final static String URL = "http://192.168.2.5:8080"; //laptop at home
 //	final static String URL = "http://10.0.1.6:8080"; //laptop at work
 //	final static String REST_URL = URL + "/KimoWebServices/rest/KimoRest/";
 	final static String REST_URL = PROTOCOL + HOST + "/";
-	static final String NAMESPACE = "http://kimo-aalexandrakis.rhcloud.com";
+	static final String NAMESPACE = "http://kimo.aalexandrakis.com";
 	static final String SOAP_ACTION_PREFIX = "/";
 	
 	static final String SENDER_ID = "634777656523";
